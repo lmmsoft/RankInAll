dce1730186997f4783031eb041962ef1156bd183
lmmsoft <lmmsoft@126.com>
2012-01-29 20:26:12 +0800

    modify core

---
+++ RankInAll.Core/OjRankEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RankInAll.Core
+{
+    public class OjRankEntity
+    {
+        public virtual int No { get; set; }
+        public virtual string UserName { get; set; }
+        public virtual string NickName { get; set; }
+        public virtual string School { get; set; }
+        public virtual string Email { get; set; }
+        public virtual int Ac { get; set; }
+        public virtual int Submit { get; set; }
+    }
+}
